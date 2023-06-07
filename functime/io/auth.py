da0ed313af37c8ac05fb711da0ebb5da9af5d75f
Daryl Lim <daryllimyt@gmail.com>
2023-06-07 04:42:06 +0100

    feat: Update cli to use functime http2 client

--- functime/io/auth.py
+++ functime/io/auth.py
@@ -1,37 +1,6 @@
-import time
-from functools import wraps
-
 import httpx
 
 from functime.config import FUNCTIME_SERVER_URL, _read_user_config, _store_user_config
-from functime.log import get_logger
-
-MAX_RETRIES = 3
-
-logger = get_logger(__name__)
-
-
-def require_token(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = get_access_token()
-        for retry in range(MAX_RETRIES):
-            try:
-                return f(token, *args, **kwargs)
-            except httpx.ReadTimeout:
-                # exponential backoff
-                timeout = 2**retry
-                logger.info(f"Retrying in {timeout} seconds...")
-                time.sleep(timeout)
-            except httpx.HTTPError as e:
-                if e.response.status_code != 401:
-                    logger.error(e)
-                    raise e
-                token = get_access_token(use_cache=False)
-
-        raise httpx.HTTPError(f"Failed to authenticate after {MAX_RETRIES} tries.")
-
-    return decorated
 
 
 def get_access_token(use_cache: bool = True):
