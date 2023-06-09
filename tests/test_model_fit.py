6544fe341e6ce689b4277039c03bd942642ba78f
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-06-09 20:28:46 +0800

    test: Consolidate tests into single module

--- tests/test_model_fit.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-from functime.forecasting import LinearModel
-
-y = pl.read_parquet("./data/m4_1w_train.parquet")
-y = y.with_columns(pl.col("series").cast(pl.Categorical))
-model = LinearModel(lags=12).fit(y=y)
-print(model.stub_id)
--- tests/test_model_fit_predict.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-from functime.forecasting import AutoLightGBM
-
-y = pl.read_parquet("./data/m4_1w_train.parquet")
-y = y.with_columns(pl.col("series").cast(pl.Categorical))
-forecaster = AutoLightGBM(freq=None).fit_predict(fh=30, y=y)
-print(forecaster)
