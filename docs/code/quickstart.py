6068a12ebe23694113d578b7b83a283f350926bf
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-06-10 06:58:03 +0800

    docs: Change artifacts method and add strategies to quickstart

--- docs/code/quickstart.py
+++ docs/code/quickstart.py
@@ -5,7 +5,7 @@ import polars as pl
 
 from functime.cross_validation import train_test_split
 from functime.feature_extraction import add_calendar_effects, add_holiday_effects
-from functime.forecasting import AutoLightGBM
+from functime.forecasting import AutoLightGBM, LightGBM
 from functime.metrics import mase
 
 start_time = default_timer()
@@ -29,10 +29,15 @@ freq = "1mo"
 y_train, y_test = train_test_split(test_size)(y)
 X_train, X_test = train_test_split(test_size)(X)
 
-# Univariate time-series fit with automated lags
+# Univariate AutoML time-series fit with automated lags
 # and hyperparameter tuning
 forecaster = AutoLightGBM(
-    freq=freq, test_size=test_size, min_lags=20, max_lags=24, n_splits=3, time_budget=10
+    freq=freq,
+    test_size=test_size,
+    min_lags=20,
+    max_lags=24,
+    n_splits=3,
+    time_budget=10,
 )
 forecaster.fit(y=y_train)
 # Predict
@@ -42,16 +47,12 @@ scores = mase(y_true=y_test, y_pred=y_pred, y_train=y_train)
 print("✅ Predictions (univariate):\n", y_pred.sort(entity_col))
 print("💯 Scores (univariate):\n", scores)
 
-# Retrieve "artifacts"
-artifacts = forecaster.state.artifacts
-best_params = artifacts["best_params"]
-print(f"✨ Best parameters (y only):\n{json.dumps(artifacts['best_params'], indent=4)}")
+# Retrieve AutoML "artifacts"
+best_params = forecaster.best_params
+print(f"✨ Best parameters (y only):\n{json.dumps(best_params, indent=4)}")
 
-
-# With exogenous features
-forecaster = AutoLightGBM(
-    test_size=test_size, freq=freq, min_lags=20, max_lags=24, n_splits=3, time_budget=10
-)
+# Multivariate non-AutoML
+forecaster = LightGBM(**best_params)
 forecaster.fit(y=y_train)
 # Predict
 y_pred = forecaster.predict(fh=test_size)
@@ -61,15 +62,17 @@ scores = mase(y_true=y_test, y_pred=y_pred, y_train=y_train)
 print("✅ Predictions (with X):\n", y_pred.sort(entity_col))
 print("💯 Scores (with X):\n", scores)
 
-# Retrieve "artifacts"
-artifacts = forecaster.state.artifacts
-best_params = artifacts["best_params"]
-print(f"✨ Best parameters (with X):\n{json.dumps(best_params, indent=4)}")
+# Direct strategy forecasting
+forecaster = LightGBM(**best_params, max_horizons=test_size, strategy="direct")
+y_pred = forecaster.predict(fh=test_size)
 
-elapsed_time = default_timer() - start_time
-print(f"⏱️ Elapsed time: {elapsed_time}")
-
+# Ensemble strategy forecasting
+forecaster = LightGBM(**best_params, max_horizons=test_size, strategy="ensemble")
+y_pred = forecaster.predict(fh=test_size)
 
 # Load fitted forecaster from deployment
 fitted_forecaster = AutoLightGBM.from_deployed(stub_id=forecaster.stub_id)
 y_pred = fitted_forecaster.predict(fh=test_size)
+
+elapsed_time = default_timer() - start_time
+print(f"⏱️ Elapsed time: {elapsed_time}")
