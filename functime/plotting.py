bf2580abfae2ccf7726f9ff1f58ee00209f30208
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-07-11 15:46:53 +0100

    feat: Add regression test

--- functime/plotting.py
+++ functime/plotting.py
@@ -2,12 +2,13 @@ import numpy as np
 import plotly.express as px
 
 
-def plot_scatter(X: np.ndarray, y: np.ndarray):
+def plot_scatter(X: np.ndarray, y: np.ndarray, dtype):
+    dtype = dtype or str
     fig = px.scatter_3d(
         x=X[:, 0],
         y=X[:, 1],
         z=X[:, 2],
-        color=y.squeeze().astype(str),  # Discrete colors
+        color=y.squeeze().astype(dtype),  # Discrete colors
         opacity=0.5
     )
     fig.update_traces(marker_size=3)
