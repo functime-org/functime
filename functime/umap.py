5e46091fe28523c80ed94d4088a41be6a844aed4
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-07-13 17:09:19 +0100

    feat: Support supervised umap

--- functime/umap.py
+++ functime/umap.py
@@ -6,15 +6,18 @@ from typing import Optional
 
 def auto_umap(
     X: np.ndarray,
+    y: Optional[np.ndarray] = None,
+    n_dims: int = 3,
     n_neighbors: int = 3,
     min_dist: float = 0.1,
-    n_dims: int = 3,
+    random_state: Optional[int] = None
 ) -> np.ndarray:
     model = umap.UMAP(
         n_components=n_dims,
         n_neighbors=n_neighbors,
         min_dist=min_dist,
         metric="manhattan",
+        random_state=random_state,
     )
-    embs = model.fit_transform(X=X)
+    embs = model.fit_transform(X=X, y=y)
     return embs
