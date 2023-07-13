7297eaaae05ca8129178fc8aec754f2bb2fb7e84
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-07-13 17:09:33 +0100

    feat: Support names in plot_scatter

--- functime/plotting.py
+++ functime/plotting.py
@@ -2,14 +2,15 @@ import numpy as np
 import plotly.express as px
 
 
-def plot_scatter(X: np.ndarray, y: np.ndarray, dtype):
+def plot_scatter(X: np.ndarray, y: np.ndarray, names=None, dtype=None):
     dtype = dtype or str
     fig = px.scatter_3d(
         x=X[:, 0],
         y=X[:, 1],
         z=X[:, 2],
         color=y.squeeze().astype(dtype),  # Discrete colors
-        opacity=0.5
+        opacity=0.5,
+        hover_name=names,
     )
     fig.update_traces(marker_size=3)
     return fig
