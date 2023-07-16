bd0be4470d5d6e793f8751bd6c15af4b9514ec68
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-07-16 12:19:31 +0100

    feat: Implement ANN forecaster with Lance

--- functime/forecasting/knn.py
+++ functime/forecasting/knn.py
@@ -7,12 +7,12 @@ from functime.forecasting._ar import fit_autoreg
 from functime.forecasting._regressors import StandardizedSklearnRegressor
 
 
-def _knn(lags: int, **kwargs):
+def _knn(**kwargs):
     def regress(X: pl.DataFrame, y: pl.DataFrame):
         from sklearn.neighbors import KNeighborsRegressor
 
         regressor = StandardizedSklearnRegressor(
-            estimator=KNeighborsRegressor(**kwargs, n_neighbors=lags, n_jobs=-1),
+            estimator=KNeighborsRegressor(**kwargs, n_jobs=-1),
         )
         return regressor.fit(X=X, y=y)
 
@@ -23,7 +23,7 @@ class knn(Forecaster):
     """Autoregressive k-nearest neighbors."""
 
     def _fit(self, y: pl.LazyFrame, X: Optional[pl.LazyFrame] = None):
-        regress = _knn(lags=self.lags, **self.kwargs)
+        regress = _knn(**self.kwargs)
         return fit_autoreg(
             regress=regress,
             y=y,
