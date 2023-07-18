d24e0713a15bf30c902cc49333a1c8b398bdaf05
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-07-18 17:33:19 +0100

    fix: Incorrect freq to sp mapping
    Resolves #11

--- functime/offsets.py
+++ functime/offsets.py
@@ -16,66 +16,45 @@ def _strip_freq_alias(freq: str) -> Tuple[int, str]:
             return offset_n, offset_alias
 
 
-def freq_to_sp(freq: str, include_dec: bool = False) -> Union[List[int], List[float]]:
+def freq_to_sp(freq: str) -> Union[List[int], List[float]]:
     """Return seasonal periods given offset alias.
+
+    Reference: https://robjhyndman.com/hyndsight/seasonal-periods/
 
     Parameters
     ----------
     freq : str
-        Offset alias supported by Polars.
-
-        The offset is dictated by the following string language:\n
-        - 1ns (1 nanosecond)
-        - 1us (1 microsecond)
-        - 1ms (1 millisecond)
+        Supported offset aliases:\n
         - 1s (1 second)
         - 1m (1 minute)
+        - 30m (30 minute)
         - 1h (1 hour)
         - 1d (1 day)
         - 1w (1 week)
         - 1mo (1 calendar month)
-        - 1q (1 calendar quarter)
+        - 3mo (1 calendar quarter)
         - 1y (1 calendar year)
-        - 1i (1 index count)
-    include_dec : bool
-        If True, return floating point seasonal periods.
-        Otherwise, all seasonal periods are rounded down
-        to the nearest integer.
 
     Returns
     -------
-    sp : list of int, list of float
+    sp : list of int
     """
 
-    # Seasonal periods for given frequency as suggested by:
-    # https://robjhyndman.com/hyndsight/seasonal-periods/
-
-    alias_to_sp = {
-        "s": [7.0, 365.25],
-        "m": [48.0, 336.0, 17532.0],
-        "30m": [60.0, 1440.0, 10080.0, 525960.0],
-        "h": [24.0, 168.0, 8766.0],
-        "d": [7.0, 365.25],
-        # 365.25/7 = 52.18 on average, allowing
-        # for a leap year every fourth year
-        "w": [52.18],
-        "y": [1.0],
-        "mo": [12.0],
-        "3mo": [4.0],
+    seasonal_periods = {
+        "1s": [60, 3_600, 86_400, 604_800, 31_557_600],
+        "1m": [60, 1_440, 10_080, 525_960],
+        "30m": [48, 336, 17_532],
+        "1h": [24, 168, 8_766],
+        "1d": [7, 365],
+        "1w": [52],
+        "1mo": [12],
+        "3mo": [4],
+        "1y": [1],
     }
-    n, alias = _strip_freq_alias(freq)
-    alias = (
-        f"{n}{alias}"
-        if ((alias.endswith("m") and n == 30) or (alias.endswith("mo") and n == 3))
-        else alias
-    )
 
     try:
-        sp = alias_to_sp[alias]
+        sp = seasonal_periods[freq]
     except KeyError as exc:
         raise ValueError(f"Offset {freq!r} not supported") from exc
-
-    if not include_dec:
-        sp = list(map(int, sp))
 
     return sp
