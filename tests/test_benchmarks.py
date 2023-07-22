c185f9a8c61f42f75aa4127e5d900891f3c1cc3a
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-07-22 21:28:19 +0100

    test: Drop short series in benchmarks

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,6 +1,6 @@
 import logging
 from functools import partial
-from typing import Mapping
+from typing import List, Mapping
 
 import numpy as np
 import polars as pl
@@ -21,6 +21,82 @@ METRICS_TO_TEST = [smape, rmse, rmsse, mae, mase, mse]
 TTEST_SIG_LEVEL = 0.10  # Two tailed
 
 
+@pytest.fixture(params=[3, 8, 16, 32, 64, 128], ids=lambda x: f"lags_{x}")
+def lags_to_test(request):
+    return request.param
+
+
+def get_short_series(X: pl.LazyFrame, min_counts: int) -> pl.DataFrame:
+    entity_col, time_col = X.columns[:2]
+    short_ts = (
+        X.groupby(entity_col)
+        .agg(pl.col(time_col).count().alias("count"))
+        .filter(pl.col("count") <= min_counts)
+        .collect(streaming=True)
+    )
+    if len(short_ts) > 0:
+        n_dropped = len(short_ts)
+        n_series = X.select(entity_col).collect().n_unique()
+        logging.info("Short %s / %s time series:\n%s", n_dropped, n_series, short_ts)
+    return short_ts.get_column(entity_col)
+
+
+@pytest.fixture
+def m4_dataset_no_missing(m4_dataset, lags_to_test):
+    """M4 dataset with short time series dropped."""
+    y_train, y_test, fh, freq = m4_dataset
+    entity_col = y_train.columns[0]
+    series_to_drop = get_short_series(y_train, min_counts=lags_to_test)
+    # Filter out short series
+    y_train_new = y_train.filter(~pl.col(entity_col).is_in(series_to_drop))
+    y_test_new = y_test.filter(~pl.col(entity_col).is_in(series_to_drop))
+    return y_train_new, y_test_new, fh, freq, lags_to_test
+
+
+@pytest.fixture
+def m5_dataset_no_missing(m5_dataset, lags_to_test):
+    """M5 dataset with short time series dropped."""
+    y_train, X_train, y_test, X_test, fh, freq = m5_dataset
+    entity_col = y_train.columns[0]
+    series_to_drop = get_short_series(y_train, min_counts=lags_to_test)
+    # Filter out short series
+    y_train_new = y_train.filter(~pl.col(entity_col).is_in(series_to_drop))
+    X_train_new = X_train.filter(~pl.col(entity_col).is_in(series_to_drop))
+    y_test_new = y_test.filter(~pl.col(entity_col).is_in(series_to_drop))
+    X_test_new = X_test.filter(~pl.col(entity_col).is_in(series_to_drop))
+    return y_train_new, X_train_new, y_test_new, X_test_new, fh, freq, lags_to_test
+
+
+@pytest.fixture
+def pd_m4_dataset(m4_dataset_no_missing):
+    y_train, y_test, fh, freq, lags = m4_dataset_no_missing
+    entity_col, time_col = y_train.columns[:2]
+    pd_y_train = y_train.collect().to_pandas()
+    pd_y_test = y_test.collect().to_pandas()
+    return pd_y_train, pd_y_test, fh, freq, lags, entity_col, time_col
+
+
+@pytest.fixture
+def pd_m5_dataset(m5_dataset_no_missing):
+    y_train, X_train, y_test, X_test, fh, freq, lags = m5_dataset_no_missing
+    entity_col, time_col = y_train.columns[:2]
+    pd_y_train = y_train.collect().to_pandas()
+    pd_y_test = y_test.collect().to_pandas()
+    pd_X_train = X_train.collect().to_pandas()
+    pd_X_test = X_test.collect().to_pandas()
+    pd_X_y = pd_y_train.merge(pd_X_train, how="left", on=[entity_col, time_col])
+    return (
+        pd_X_y,
+        pd_y_test,
+        pd_X_test,
+        fh,
+        freq[-1].upper(),
+        lags,
+        entity_col,
+        time_col,
+    )
+
+
 @pytest.fixture(
     params=[
         ("linear", lambda: LinearRegression(fit_intercept=True)),
@@ -39,17 +115,28 @@ def forecaster(request):
     return request.param
 
 
-@pytest.fixture(params=[3, 8, 16, 32, 64], ids=lambda x: f"lags({x})")
-def lags_to_test(request):
-    return request.param
+def summarize_scores(scores: Mapping[str, np.ndarray]) -> Mapping[str, float]:
+    return {k: sum(arr) / len(arr) for k, arr in scores.items()}
 
 
 def score_forecasts(
     y_true: pl.DataFrame,
     y_pred: pl.DataFrame,
     y_train: pl.DataFrame,
-) -> Mapping[str, np.ndarray]:
+) -> Mapping[str, List[float]]:
     """Return mapping of metric name to scores across time-series."""
+
+    # Defensive sort and coerce time column
+    entity_col, time_col = y_true.columns[:2]
+    y_true = y_true.sort([entity_col, time_col])
+    y_pred = pl.concat(
+        [
+            y_true.select([entity_col, time_col]),
+            y_pred.sort([entity_col, time_col]).select(y_pred.columns[-1]),
+        ],
+        how="horizontal",
+    )
+
     scores = {}
     for metric in METRICS_TO_TEST:
         metric_name = metric.__name__
@@ -58,25 +145,25 @@ def score_forecasts(
         scores[metric_name] = (
             metric(y_true=y_true, y_pred=y_pred)
             .get_column(metric_name)
-            .to_numpy(zero_copy_only=True)
+            .to_list()  # So that it's JSON serializable for pytest cache
         )
     return scores
 
 
 # 6 mins timeout
 @pytest.mark.benchmark
-def test_mlforecast_on_m4(pd_m4_dataset, regressor, lags_to_test, benchmark, request):
+def test_mlforecast_on_m4(regressor, pd_m4_dataset, benchmark, request):
     from joblib import cpu_count
     from mlforecast import MLForecast
     from mlforecast.target_transforms import LocalStandardScaler
 
-    y_train, y_test, fh, freq, entity_col, time_col = pd_m4_dataset
+    y_train, y_test, fh, freq, lags, entity_col, time_col = pd_m4_dataset
     estimator_name, estimator = regressor
 
     def fit_predict():
         model = MLForecast(
             models=[estimator()],
-            lags=list(range(lags_to_test)),
+            lags=list(range(lags)),
             num_threads=cpu_count(),
             target_transforms=[LocalStandardScaler()],
         )
@@ -95,30 +182,28 @@ def test_mlforecast_on_m4(pd_m4_dataset, regressor, lags_to_test, benchmark, req
         y_pred=pl.DataFrame(y_pred),
         y_train=pl.DataFrame(y_train),
     )
-    cache_id = f"m4_{freq}_{lags_to_test}_{estimator_name}"
     logging.info(
-        "mlforecast M4 (freq={freq}, lags={lags}) scores:\n{scores}",
-        freq=freq,
-        lags=lags_to_test,
-        scores=scores,
+        "Baseline scores (freq=%s, lags=%s): %s", freq, lags, summarize_scores(scores)
     )
+
+    cache_id = f"baseline_m4_{freq}_{lags}_{estimator_name}"
     request.config.cache.set(cache_id, scores)
 
 
 @pytest.mark.benchmark
-def test_mlforecast_on_m5(pd_m5_dataset, regressor, lags_to_test, benchmark, request):
+def test_mlforecast_on_m5(regressor, pd_m5_dataset, benchmark, request):
     from joblib import cpu_count
     from mlforecast import MLForecast
     from mlforecast.target_transforms import LocalStandardScaler
 
-    X_y_train, X_y_test, X_test, fh, entity_col, time_col = pd_m5_dataset
+    X_y_train, X_y_test, X_test, fh, freq, lags, entity_col, time_col = pd_m5_dataset
     estimator_name, estimator = regressor
 
     def fit_predict():
         model = MLForecast(
             models=[estimator],
-            freq="D",
-            lags=list(range(lags_to_test)),
+            freq=freq,
+            lags=list(range(lags)),
             num_threads=cpu_count(),
             target_transforms=[LocalStandardScaler()],
         )
@@ -127,7 +212,7 @@ def test_mlforecast_on_m5(pd_m5_dataset, regressor, lags_to_test, benchmark, req
             id_col=entity_col,
             time_col=time_col,
             target_col="quantity_sold",
-            keep_last_n=lags_to_test,
+            keep_last_n=lags,
         )
         y_pred = model.predict(fh, dynamic_dfs=[X_test])
         return y_pred
@@ -138,56 +223,58 @@ def test_mlforecast_on_m5(pd_m5_dataset, regressor, lags_to_test, benchmark, req
         y_pred=pl.DataFrame(y_pred),
         y_train=pl.DataFrame(X_y_train.loc[:, entity_col, time_col, "quantity_sold"]),
     )
-    cache_id = f"mlforecast_m5_{lags_to_test}_{estimator_name}_scores"
+    logging.info("Baseline scores (lags=%s): %s", lags, summarize_scores(scores))
+
+    cache_id = f"baseline_m5_{lags}_{estimator_name}_scores"
     request.config.cache.set(cache_id, scores)
 
 
 @pytest.mark.benchmark
-def test_functime_on_m4(m4_dataset, forecaster, lags_to_test, benchmark, request):
-    y_train, y_test, fh, freq = m4_dataset
+def test_functime_on_m4(forecaster, m4_dataset_no_missing, benchmark, request):
+    y_train, y_test, fh, freq, lags = m4_dataset_no_missing
     model_name, model = forecaster
     y_pred = benchmark(
-        lambda: model(lags=lags_to_test, freq=freq, target_transform=scale())(
-            y=y_train, fh=fh
-        )
+        lambda: model(lags=lags, freq=freq, target_transform=scale())(y=y_train, fh=fh)
     )
 
     # Score forceasts
-    scores = score_forecasts(y_true=y_test, y_pred=y_pred, y_train=y_train)
+    scores = score_forecasts(y_true=y_test.collect(), y_pred=y_pred, y_train=y_train)
     mlforecast_scores = request.config.cache.get(
-        f"m4_{freq}_{lags_to_test}_{model_name}"
+        f"baseline_m4_{freq}_{lags}_{model_name}", None
     )
 
     for metric_name, baseline_scores in mlforecast_scores.items():
-        # Compare mean scores with t-test
-        res = ttest_ind(a=scores[metric_name], b=baseline_scores)
-        assert res.pvalue > TTEST_SIG_LEVEL
+        functime_scores = scores[metric_name]
+        mean_functime_score = np.mean(functime_scores)
+        mean_baseline_score = np.mean(baseline_scores)
+        if mean_functime_score > mean_baseline_score:
+            assert len(functime_scores) == len(baseline_scores)
+            res = ttest_ind(a=functime_scores, b=baseline_scores)
+            assert res.pvalue > TTEST_SIG_LEVEL
 
 
 @pytest.mark.benchmark
-def test_functime_on_m5(m5_dataset, forecaster, lags_to_test, benchmark, request):
-    y_train, X_train, y_test, X_test, fh, freq = m5_dataset
+def test_functime_on_m5(forecaster, m4_dataset_no_missing, benchmark, request):
+    y_train, X_train, y_test, X_test, fh, freq, lags = m4_dataset_no_missing
     model_name, model = forecaster
     y_pred = benchmark(
-        lambda: model(lags=lags_to_test, freq=freq, target_transform=scale())(
+        lambda: model(lags=lags, freq=freq, target_transform=scale())(
             y=y_train, X_train=X_train, X_future=X_test, fh=fh
         )
     )
 
-    # Check no missing time-series
-    entity_col = y_pred.columns[0]
-    assert pl.testing.assert_frame_equal(
-        y_train.select(pl.col(entity_col).unique()).collect(),
-        y_pred.select(pl.col(entity_col).unique()).collect(),
-    )
-
     # Score forceasts
-    scores = score_forecasts(y_true=y_test, y_pred=y_pred)
-
+    scores = score_forecasts(y_true=y_test.collect(), y_pred=y_pred)
     mlforecast_scores = request.config.cache.get(
-        f"mlforecast_m5_{lags_to_test}_{model_name}_scores"
+        f"baseline_m5_{lags_to_test}_{model_name}_scores", None
     )
+
     for metric_name, baseline_scores in mlforecast_scores.items():
         # Compare mean scores with t-test
-        res = ttest_ind(a=scores[metric_name], b=baseline_scores)
-        assert res.pvalue > TTEST_SIG_LEVEL
+        functime_scores = scores[metric_name]
+        mean_functime_score = np.mean(functime_scores)
+        mean_baseline_score = np.mean(baseline_scores)
+        if mean_functime_score > mean_baseline_score:
+            assert len(functime_scores) == len(baseline_scores)
+            res = ttest_ind(a=functime_scores, b=baseline_scores)
+            assert res.pvalue > TTEST_SIG_LEVEL
