c9c4b53f7162dadcb6391fdbcc93c8b750b8837f
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-08-09 00:55:48 +0100

    docs: Update quickstart with uplift

--- docs/code/quickstart.py
+++ docs/code/quickstart.py
@@ -4,13 +4,9 @@ from timeit import default_timer
 import polars as pl
 
 from functime.cross_validation import train_test_split
-from functime.feature_extraction import (
-    add_calendar_effects,
-    add_fourier_terms,
-    add_holiday_effects,
-)
-from functime.forecasting import auto_lightgbm, lightgbm
-from functime.metrics import mase
+from functime.feature_extraction import add_fourier_terms
+from functime.forecasting import auto_linear_model, linear_model
+from functime.metrics import smape
 from functime.preprocessing import scale
 
 start_time = default_timer()
@@ -20,12 +16,7 @@ y = pl.read_parquet(
     "https://github.com/descendant-ai/functime/raw/main/data/commodities.parquet"
 )
 entity_col, time_col = y.columns[:2]
-X = (
-    y.select([entity_col, time_col])
-    .pipe(add_calendar_effects(["month"]))
-    .pipe(add_holiday_effects(country_codes=["US"]))
-    .collect()
-)
+X = y.select([entity_col, time_col]).pipe(add_fourier_terms(sp=12, K=6)).collect()
 
 print("🎯 Target variable (y):\n", y)
 print("📉 Exogenous variables (X):\n", X)
@@ -37,50 +28,66 @@ y_train, y_test = train_test_split(test_size)(y)
 X_train, X_test = train_test_split(test_size)(X)
 
 # Univariate time-series fit with automated lags and hyperparameter tuning
-auto_forecaster = auto_lightgbm(
-    freq=freq,
-    test_size=test_size,
-    min_lags=20,
-    max_lags=24,
-    n_splits=3,
-    time_budget=10,
+auto_forecaster = auto_linear_model(
+    freq=freq, test_size=test_size, min_lags=12, max_lags=18, n_splits=3, time_budget=3
 )
 auto_forecaster.fit(y=y_train)
 # Predict
 y_pred = auto_forecaster.predict(fh=test_size)
 # Score
-scores = mase(y_true=y_test, y_pred=y_pred, y_train=y_train)
+scores = smape(y_true=y_test, y_pred=y_pred)
 print("✅ Predictions (univariate):\n", y_pred.sort(entity_col))
-print("💯 Scores (univariate):\n", scores)
+print("💯 Scores (univariate):\n", scores.sort("smape"))
+print("💯 Scores summary (univariate):\n", scores.select("smape").describe())
 
 # Retrieve best lags and hyperparameters
 best_params = auto_forecaster.best_params
 print(f"✨ Best parameters (y only):\n{json.dumps(best_params, indent=4)}")
 
 # Multivariate
-forecaster = lightgbm(**best_params)
-forecaster.fit(y=y_train)
+forecaster = linear_model(**best_params)
+forecaster.fit(y=y_train, X=X_train)
 # Predict
-y_pred = forecaster.predict(fh=test_size)
+y_pred = forecaster.predict(fh=test_size, X=X_test)
 # Score
-scores = mase(y_true=y_test, y_pred=y_pred, y_train=y_train)
+scores_with_exog = smape(y_true=y_test, y_pred=y_pred)
 
-print("✅ Predictions (with X):\n", y_pred.sort(entity_col))
-print("💯 Scores (with X):\n", scores)
+print("✅ Predictions (multivariate):\n", y_pred.sort(entity_col))
+print("💯 Scores (multivariate):\n", scores_with_exog.sort("smape"))
+print("💯 Scores summary (multivariate):\n", scores_with_exog.select("smape").describe())
+
+# Check uplift from Fourier features
+uplift = (
+    scores_with_exog.join(scores, on=entity_col, suffix="_univar")
+    .with_columns(
+        uplift=pl.col("smape_univar") - pl.col("smape"),
+        has_uplift=pl.col("smape_univar") - pl.col("smape") > 0,
+    )
+    .select([entity_col, "uplift", "has_uplift"])
+)
+
+# NOTE: Fourier features lead to uplift for ~20% of commodities
+# However, at the expense of an overall mean and variance SMAPE
+# (likely due to overfitting on seasonal features)
+
+print("💯 Uplift:\n", uplift.sort("uplift", descending=True))
+print("💯 Proportion with uplift:", uplift.get_column("has_uplift").mean())
 
 # "Direct" strategy forecasting
 best_params["max_horizons"] = test_size  # Override max_horizons
 best_params["strategy"] = "direct"  # Override strategy
 # Predict using the "functional" API
-y_pred = lightgbm(**best_params)(y=y_train, fh=test_size)
+y_pred = linear_model(**best_params)(y=y_train, fh=test_size)
 
 # "Ensemble" strategy forecasting
 best_params["strategy"] = "ensemble"  # Override strategy
-# Predict using the "functional" API
-y_pred = lightgbm(**best_params)(y=y_train, fh=test_size)
+
+# Backtesting
+y_preds = linear_model(**best_params).backtest(y=y_train, X=X_train)
+print("✅ Backtests:", y_preds)
 
 # Forecast with target transforms and feature transforms
-forecaster = lightgbm(
+forecaster = linear_model(
     freq="1mo",
     lags=24,
     target_transform=scale(),
