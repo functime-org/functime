785e8fd680501f10c452174e1c04b885d38c831a
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-08-28 11:29:56 +0100

    feat!: Move panel to wide preproc into llm context

--- functime/llm/api.py
+++ functime/llm/api.py
@@ -1,4 +1,4 @@
-from typing import Mapping, Optional, Union
+from typing import List, Optional
 
 import polars as pl
 
@@ -13,14 +13,35 @@ class LLMActions:
     def __init__(self, df: pl.DataFrame):
         self._df = df
 
+    def _filter_panel_entities(self, panel_df: pl.DataFrame, basket: List[str]):
+        entity_col = panel_df.columns[0]
+        df = panel_df.filter(pl.col(entity_col).is_in(basket))
+        if df.is_empty():
+            raise ValueError(
+                f"No matching entities found in panel given basket: {basket}"
+            )
+        return df
+
+    def _panel_to_wide(self, panel_df: pl.DataFrame) -> pl.DataFrame:
+        entity_col, time_col, target_col = panel_df.columns[:3]
+        wide_df = panel_df.pivot(
+            index=time_col,
+            values=target_col,
+            columns=entity_col,
+            aggregate_function=None,
+            sort_columns=True,
+        ).sort(time_col)
+        return wide_df
+
     def analyze(
         self,
+        basket: List[str],
         context: Optional[str] = None,
         model: MODEL_T = "gpt-3.5-turbo",
         format: FORMAT_T = "markdown_bullet_list",
         **kwargs,
     ) -> str:
-        """Analyze a polars DataFrame."""
+        """Analyze a `basket` (list of entity / series IDs) of forecasts."""
 
         task, formatting = format_instructions(format)
         prompt_context = f" The context is: {context}."
@@ -29,11 +50,13 @@ class LLMActions:
             " Describe trend, seasonality, and anomalies. Do not provide recommendations. Do not describe the table."
             " Do not introduce yourself or your role."
         )
+        forecasts = self._filter_panel_entities(self.df, basket=basket)
+        wide_forecasts = self._panel_to_wide(forecasts)
         prompt = (
             f"{task}"
             f"{prompt_context}"
             f"{constraints}"
-            f"{format_dataframes(self._df)}"
+            f"{format_dataframes(wide_forecasts)}"
             f"{formatting}"
         )
         response = openai_call(prompt, model, **kwargs)
@@ -41,14 +64,13 @@ class LLMActions:
 
     def compare(
         self,
-        others: Union[pl.DataFrame, Mapping[str, pl.DataFrame]],
-        *,
-        as_label: str = "This",
+        basket: List[str],
+        other_basket: List[str],
         model: MODEL_T = "gpt-3.5-turbo",
         target_feature: Optional[str] = None,
         **kwargs,
     ) -> str:
-        """Compare 2 or more polars DataFrames."""
+        """Compare two basket of forecasts."""
 
         task = "Compare and contrast the following time series data."
 
@@ -62,16 +84,16 @@ class LLMActions:
             " Do not introduce yourself or your role."
         )
         output_format = "{{ Insert your comparative analysis here }}"
-        dfs = {as_label: self._df}
-        if isinstance(others, pl.DataFrame):
-            dfs.update({"other": others})
-        else:
-            dfs.update(others)
+        forecasts = {
+            "This": self._filter_panel_entities(self._df, basket=basket),
+            "Other": self._filter_panel_entities(self._df, basket=other_basket),
+        }
+        wide_forecasts = {k: self._panel_to_wide(df) for k, df in forecasts.items()}
         prompt = (
             f"{task}"
             f"{prompt_context}"
             f"{constraints}"
-            f"{format_dataframes(dfs)}"
+            f"{format_dataframes(wide_forecasts)}"
             f"{output_format}"
         )
         response = openai_call(prompt, model, **kwargs)
