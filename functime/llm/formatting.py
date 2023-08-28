15233dd6f5319d760a02f27f55170df32d125a50
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-08-28 11:29:56 +0100

    feat!: Move panel to wide preproc into llm context

--- functime/llm/formatting.py
+++ functime/llm/formatting.py
@@ -1,4 +1,4 @@
-from typing import Mapping, Optional, Tuple, Union
+from typing import Mapping, Tuple, Union
 
 import polars as pl
 from typing_extensions import Literal
@@ -31,32 +31,3 @@ def format_instructions(format: FORMAT_T) -> Tuple[str, str]:
             "{{ Insert your response here }}",
         )
     raise ValueError(f"Invalid formatting option: {format}")
-
-
-def univariate_panel_to_wide(
-    panel_df: pl.DataFrame,
-    *,
-    entity_col: Optional[str] = None,
-    time_col: Optional[str] = None,
-    target_col: Optional[str] = None,
-    drop_nulls: bool = False,
-    shrink_dtype: bool = False,
-) -> pl.DataFrame:
-    if entity_col is None:
-        entity_col = panel_df.columns[0]
-    if time_col is None:
-        time_col = panel_df.columns[1]
-    if target_col is None:
-        target_col = panel_df.columns[2]
-    wide_df = panel_df.pivot(
-        index=time_col,
-        values=target_col,
-        columns=entity_col,
-        aggregate_function=None,
-        sort_columns=True,
-    ).sort(time_col)
-    if drop_nulls:
-        wide_df = wide_df.drop_nulls()
-    if shrink_dtype:
-        wide_df = wide_df.select(pl.all().shrink_dtype())
-    return wide_df
