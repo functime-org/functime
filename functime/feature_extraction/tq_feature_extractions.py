592553425e7e7b7a170d5b313a74eb9d7d7e6937
Tianren Qin <qtren2015@hotmail.com>
2023-09-17 15:05:27 -0400

    added query similarity count

--- functime/feature_extraction/tq_feature_extractions.py
+++ functime/feature_extraction/tq_feature_extractions.py
@@ -3,14 +3,17 @@ from scipy.spatial import KDTree
 import numpy as np
 import math
 
-# permutation_entropy: done, test?
-# query_similarity_count: not yet
-# range_count: done, test?
-# ratio_beyond_r_sigma: done, test?
-# ratio_value_number_to_length (ratio_n_unique_to_length): done, test?
-# root_mean_square: done, test?
-# sample_entropy: done, test?
+# permutation_entropy: done, tested against tsfresh
+# query_similarity_count: done, but not sure if this is correct...
+# range_count: done, tested
+# ratio_beyond_r_sigma: done, don't think I need to test this
+# ratio_value_number_to_length (ratio_n_unique_to_length): done, don't think I need to test this
+# root_mean_square: done, don't think I need to test this
+# sample_entropy: done, tested against tsfresh
 # spkt_welch_density ( Is this fourier related?): not yet
+
+def spkt_welch_density():
+    pass
 
 def ratio_n_unique_to_length(x: pl.Expr):
     return x.n_unique() / x.len()
@@ -25,7 +28,7 @@ def root_mean_square(x: pl.Expr) -> pl.Expr:
         The input expression
     '''
     # Try (x.dot(x) / pl.count()).sqrt() and compare performance.
-    # dot generally has pretty good performance
+    # Using dot is usually a few micro-seconds faster
     return (x**2).mean().sqrt()
 
 def range_count(x: pl.Expr, lower:float, upper:float) -> pl.Expr:
@@ -41,15 +44,53 @@ def range_count(x: pl.Expr, lower:float, upper:float) -> pl.Expr:
     upper : float
         The upper bound, exclusive
     '''
+    if upper < lower:
+        raise ValueError("Upper must be greater than lower.")
     return x.is_between(lower_bound=lower, upper_bound=upper, closed="left").sum()
 
-# I don't understand the doc for this.
-def query_similarity_count(x:pl.Expr):
-    pass
+# I have no idea how to test this. This is purely based on my understanding of what I read
+# from tsfresh. I do not know how to use tsfresh's query_similarity_count either.
+def query_similarity_count(x:pl.Expr, subseq:list[float], threshold:float, z_normalize:bool=False):
+    '''
+    Count how many subsequences of x are similar to the given subseq. Euclidean distance is used.
+
+    Parameters
+    ----------
+    x : pl.Expr
+        The input expression
+    subseq : list[float] | Series
+        A list of floats or a Polars series to compare with
+    threshold : float
+        If distance is within threshold, it is considered a similar match.
+    z_normalize: bool
+        Whether to z_normalize before comparing.
+    '''
+    # If we want to allow other distances metrics, one way 
+    # is to change pl.element().pow(2).sum().sqrt() step
+
+    s = pl.Series(subseq, dtype=pl.Float64)
+    if z_normalize:
+        s = (s - s.mean())/s.std()
+        mean = pl.concat_list(x, *(x.shift(-i) for i in range(1, len(s)))).list.eval(pl.element().mean()).list.first()
+        std = pl.concat_list(x, *(x.shift(-i) for i in range(1, len(s)))).list.eval(pl.element().std()).list.first()
+        expr = (
+            pl.concat_list((x-mean)/std - pl.lit(s[0]), *(((x.shift(-i)-mean)/std - pl.lit(s[i])) 
+                                                          for i in range(1, len(s))))
+            .list.eval(pl.element().pow(2).sum().sqrt())
+            .list.first().lt(threshold).sum()
+        )
+    else:
+        expr = (
+            pl.concat_list(x - s[0], *(x.shift(-i) - s[i] for i in range(1, len(s))))
+            .list.eval(pl.element().pow(2).sum().sqrt())
+            .list.first().lt(threshold).sum()
+        )
+
+    return expr
 
 def ratio_beyond_r_sigma(x: pl.Expr, r: float) -> pl.Expr:
     '''
-    Returns the ration of values in the series that is beyond r*std from mean on both sides.
+    Returns the ratio of values in the series that is beyond r*std from mean on both sides.
 
     Parameters
     ----------
@@ -63,7 +104,7 @@ def ratio_beyond_r_sigma(x: pl.Expr, r: float) -> pl.Expr:
             x.mean() - pl.lit(r) * x.std(),
             x.mean() + pl.lit(r) * x.std(),
             closed="both"
-        ).is_not()
+        ).is_not() # check for deprecation
         .sum()
         / pl.count()
     )
@@ -83,7 +124,6 @@ def _into_sequential_chunks(x:pl.Series, m:int) -> np.ndarray:
 
 def sample_entropy(x: pl.Series, r:float=0.2) -> float:
     # This is significantly faster than tsfresh
-
     threshold = r * x.std(ddof=0)
     m = 2
     mat = _into_sequential_chunks(x, m)
@@ -122,18 +162,16 @@ def permutation_entropy(
         https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.permutation_entropy
     '''
 
-    # CSE should take care of x.shift(-n_dims+1).is_not_null() ?
-    # If input is eager, then in the divide statement we don't need need
-    # a lazy expression to compute the remaining length.
+    # This is kind of slow rn when tau > 1
     max_shift = -n_dims + 1
     out = (
         pl.concat_list(x, *(x.shift(-i) for i in range(1,n_dims))) # create list columns
         .take_every(tau) # take every tau
-        .filter(x.shift(max_shift).is_not_null()) # This is a filter because length of df is unknown
+        .filter(x.shift(max_shift).take_every(tau).is_not_null()) # This is a filter because length of df is unknown, might slow down perf
         .list.eval(pl.element().rank(method="ordinal")) # for each inner list, do an argsort
         .value_counts() # groupby and count, but returns a struct
         .struct.field("counts") # extract the field named "counts"
-        / x.shift(max_shift).is_not_null().sum() # get probabilities, alt:(pl.count() - max_shift)
+        / x.shift(max_shift).take_every(tau).is_not_null().sum() # get probabilities
     ).entropy(base=base, normalize=normalize).suffix("_permutation_entropy")
 
     return out
--- tests/test_tq.py
+++ tests/test_tq.py
@@ -7,7 +7,7 @@ import functime.feature_extraction.tq_feature_extractions as ft
 COL = "price"
 
 @pytest.mark.parametrize("execution_times", [5])
-def test_sample_entropy_tsfresh(execution_times:int):
+def test_sample_entropy_against_tsfresh(execution_times:int):
     test_df = pl.read_parquet("data/commodities.parquet")
     for _ in range(execution_times):
         df = test_df.sample(n=2000)
@@ -18,3 +18,53 @@ def test_sample_entropy_tsfresh(execution_times:int):
         rewrite = round(float(ft.sample_entropy(pl_s, r = 0.2)), 12)
 
         assert tsfresh == rewrite
+
+def test_permutation_entropy_against_tsfresh():
+    test_df = pl.read_parquet("data/commodities.parquet")
+
+    numpy_s = test_df[COL].to_numpy()
+
+    tsfresh1 = round(fc.permutation_entropy(numpy_s, tau=1, dimension=3), 12)
+    rewrite1 = test_df.select(
+        ft.permutation_entropy(pl.col(COL), tau=1, n_dims=3)
+    ).item(0,0)
+    rewrite1 = round(rewrite1, 12)
+    assert tsfresh1 == rewrite1
+
+    tsfresh2 = round(fc.permutation_entropy(numpy_s, tau=2, dimension=4), 12)
+    rewrite2 = test_df.select(
+        ft.permutation_entropy(pl.col(COL), tau=2, n_dims=4)
+    ).item(0,0)
+    rewrite2 = round(rewrite2, 12)
+    assert tsfresh2 == rewrite2
+
+def test_range_count_against_tsfresh():
+    test_df = pl.read_parquet("data/commodities.parquet")
+
+    numpy_s = test_df[COL].to_numpy()
+
+    tsfresh1 = round(fc.range_count(numpy_s, min=500, max=1000), 12)
+    rewrite1 = test_df.select(
+        ft.range_count(pl.col(COL), lower=500, upper=1000)
+    ).item(0,0)
+    rewrite1 = round(rewrite1, 12)
+    assert tsfresh1 == rewrite1
+
+    tsfresh2 = round(fc.range_count(numpy_s, min=1200, max=1300), 12)
+    rewrite2 = test_df.select(
+        ft.range_count(pl.col(COL), lower=1200, upper=1300)
+    ).item(0,0)
+    rewrite2 = round(rewrite2, 12)
+    assert tsfresh2 == rewrite2
+
+@pytest.mark.parametrize("bounds", [(5,10), (1,2), (23,25)])
+def test_range_count(bounds:tuple[int, int]):
+    test_df = pl.DataFrame({
+        "a": range(100)
+    })
+    lower = bounds[0]
+    upper = bounds[1]
+    ans = test_df.select(
+        ft.range_count(pl.col("a"), lower=lower, upper=upper)
+    ).item(0,0)
+    assert ans == upper-lower
