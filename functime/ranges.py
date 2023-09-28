deeef3a32cd9197fc275ce94735c8e1f2317ffc5
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2023-09-28 16:13:39 -0700

    refactor!: Drop support for null freq in `make_future_ranges`

--- functime/ranges.py
+++ functime/ranges.py
@@ -9,7 +9,7 @@ def make_future_ranges(
     time_col: str,
     cutoffs: pl.DataFrame,
     fh: int,
-    freq: Optional[str] = None,
+    freq: str,
     time_unit: Optional[str] = None,
 ) -> pl.DataFrame:
     """Return pl.DataFrame with columns entity_col, time_col.
@@ -17,45 +17,33 @@ def make_future_ranges(
     DataFrame has shape (n_entities, 2) and dtypes (str, list[date]), (str, list[datetime]), or (str, list[int]).
     """
     entity_col = cutoffs.columns[0]
-    if isinstance(freq, str):
-        if freq.endswith("i"):
-            future_ranges = cutoffs.select(
-                [
-                    pl.col(entity_col),
-                    pl.int_ranges(
-                        pl.col("low") + 1,
-                        pl.col("low") + fh + 1,
-                        step=int(freq[:-1]),
-                        eager=False,
-                    ).alias(time_col),
-                ]
-            )
-        else:
-            time_unit = time_unit or "us"
-            offset_n, offset_alias = _strip_freq_alias(freq)
-            # Make date ranges
-            future_ranges = cutoffs.select(
-                [
-                    pl.col(entity_col),
-                    pl.date_ranges(
-                        pl.col("low"),
-                        pl.col("low")
-                        .dt.offset_by(f"{fh * offset_n}{offset_alias}")
-                        .alias("high"),
-                        interval=freq,
-                        closed="right",
-                        time_unit=time_unit,
-                        eager=False,
-                    ).alias(time_col),
-                ]
-            )
-    else:
+    if freq.endswith("i"):
         future_ranges = cutoffs.select(
             [
                 pl.col(entity_col),
                 pl.int_ranges(
-                    pl.ones(len(cutoffs), eager=False),
-                    pl.ones(len(cutoffs), eager=False) + fh,
+                    pl.col("low") + 1,
+                    pl.col("low") + fh + 1,
+                    step=int(freq[:-1]),
+                    eager=False,
+                ).alias(time_col),
+            ]
+        )
+    else:
+        time_unit = time_unit or "us"
+        offset_n, offset_alias = _strip_freq_alias(freq)
+        # Make date ranges
+        future_ranges = cutoffs.select(
+            [
+                pl.col(entity_col),
+                pl.date_ranges(
+                    pl.col("low"),
+                    pl.col("low")
+                    .dt.offset_by(f"{fh * offset_n}{offset_alias}")
+                    .alias("high"),
+                    interval=freq,
+                    closed="right",
+                    time_unit=time_unit,
                     eager=False,
                 ).alias(time_col),
             ]
