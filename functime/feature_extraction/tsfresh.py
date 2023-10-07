7919f9d3ed9a3d118f7fa4796dcc4b8eb7facffd
Mathieu Cayssol <mathieu.cayssol@gmail.com>
2023-10-07 10:02:04 +0000

    update .not_()

--- functime/feature_extraction/tsfresh.py
+++ functime/feature_extraction/tsfresh.py
@@ -1189,7 +1189,7 @@ def ratio_beyond_r_sigma(x: TIME_SERIES_T, ratio: float = 0.25) -> FLOAT_EXPR:
             x.mean() + pl.lit(ratio) * x.std(),
             closed="both",
         )
-        .is_not()  # check for deprecation
+        .not_()  # check for deprecation
         .sum()
         / x.len()
     )
