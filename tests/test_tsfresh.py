2e283feb7626dd604029b9f37c9d0a836585058c
plaales <147873535+plaales@users.noreply.github.com>
2023-10-25 11:04:11 +0200

    Update tests/test_tsfresh.py
    Co-authored-by: Mathieu Cayssol <49449000+MathieuCayssol@users.noreply.github.com>

--- tests/test_tsfresh.py
+++ tests/test_tsfresh.py
@@ -1388,27 +1388,26 @@ def test_range_over_mean_and_range(S, res):
         df.lazy().select(range_over_mean(pl.col(x.name))).collect(),
         pl.DataFrame({x.name: [res]}),
     )
-
-
 @pytest.mark.parametrize(
-    "S, m, res"
+    "S, res, m",
     [
-        ([10, -10, 10, -10], 3, 0),
-        ([10, -10, 10, -10], 0, 10),
-        ([10, 20, 20, 30], 0, 1),
-        ([10, 20, 20, 30], 1, 15),
+        ([10, -10, 10, -10], [3], 0),
+        ([10, -10, 10, -10], [0], 10),
+        ([10, 20, 20, 30], [0], 0),
+        ([10, 20, 20, 30], [1], 15),
+        ([10, -10, 10, -10], [3], 0),
+        ([-10, 10.1, -10, 10.1, -10], [4], 10),
+        ([10,11,12,10,11], [3], 10.5)
     ],
 )
-def test_number_crossing(S, m, res):
-    assert_series_equal(
-        pl.Series("a", [number_crossings(pl.Series("a", S), m)]),
-        pl.Series("a", res, dtype=pl.float64)
-    )
+def test_number_crossing(S, res, m):
+    assert number_crossings(pl.Series(S), m) == res[0]
+    
     assert_frame_equal(
         pl.DataFrame({"a": S}).select(number_crossings(pl.col("a"), m)),
-        pl.DataFrame(pl.Series("a", res, dtype=pl.Float64)),
+        pl.DataFrame(pl.Series("a", res, pl.UInt32)),
     )
     assert_frame_equal(
         pl.LazyFrame({"a": S}).select(number_crossings(pl.col("a"), m)).collect(),
-        pl.DataFrame(pl.Series("a", res, dtype=pl.Float64)),
+        pl.DataFrame(pl.Series("a", res, pl.UInt32)),
     )
