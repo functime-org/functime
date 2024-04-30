44c0246e24b17ff650a985403276eda2ef69c1b3
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-04-30 23:35:10 +0200

    refactor: Properly annotate `train_test_split` (#210)

---
+++ functime/type_aliases.py
@@ -0,0 +1,33 @@
+"""Type aliases."""
+
+from __future__ import annotations
+
+import sys
+from typing import (
+    Callable,
+    Literal,
+    Tuple,
+    Union,
+)
+
+import polars as pl
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:  # 3.9
+    from typing_extensions import TypeAlias
+
+# general
+PolarsFrame: TypeAlias = Union[pl.DataFrame, pl.LazyFrame]
+
+# cross validation
+EagerSplitter: TypeAlias = Callable[
+    [Union[pl.DataFrame, pl.LazyFrame]], Tuple[pl.DataFrame, pl.DataFrame]
+]
+LazySplitter: TypeAlias = Callable[
+    [Union[pl.DataFrame, pl.LazyFrame]], Tuple[pl.LazyFrame, pl.LazyFrame]
+]
+
+
+# feature extraction
+DetrendMethod: TypeAlias = Literal["linear", "mean"]
