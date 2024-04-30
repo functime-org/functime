d030c703a1e73d275913a5175f934a3131b476c1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-04-30 16:58:04 +0200

    refactor: add Splitter aliases

--- functime/type_aliases.py
+++ functime/type_aliases.py
@@ -3,12 +3,31 @@
 from __future__ import annotations
 
 import sys
-from typing import Literal
+from typing import (
+    Callable,
+    Literal,
+    Tuple,
+    TypeAlias,
+    Union,
+)
+
+import polars as pl
 
 if sys.version_info >= (3, 10):
     from typing import TypeAlias
 else:  # 3.9
     from typing_extensions import TypeAlias
+
+# general
+PolarsFrame: TypeAlias = Union[pl.DataFrame, pl.LazyFrame]
+
+# cross validation
+EagerSplitter: TypeAlias = Callable[
+    [Union[pl.DataFrame, pl.LazyFrame]], Tuple[pl.DataFrame, pl.DataFrame]
+]
+LazySplitter: TypeAlias = Callable[
+    [Union[pl.DataFrame, pl.LazyFrame]], Tuple[pl.LazyFrame, pl.LazyFrame]
+]
 
 
 # feature extraction
