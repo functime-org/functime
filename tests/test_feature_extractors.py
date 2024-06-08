872d92b53af7c9fb1f76683d465da0569907ff15
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-06-08 14:44:30 +0200

    test: replace random_integers with randint

--- tests/test_feature_extractors.py
+++ tests/test_feature_extractors.py
@@ -7,11 +7,9 @@ import pytest
 from functime.feature_extractors import cwt_coefficients
 
 
-@pytest.mark.parametrize("length", np.random.random_integers(low=1, high=100, size=5))
+@pytest.mark.parametrize("length", np.random.randint(low=1, high=101, size=5))
 @pytest.mark.parametrize("widths", [(2,), (2, 5, 10, 20), (2, 5, 10, 20, 30)])
-@pytest.mark.parametrize(
-    "n_coefficients", np.random.random_integers(low=1, high=100, size=5)
-)
+@pytest.mark.parametrize("n_coefficients", np.random.randint(low=1, high=101, size=5))
 def test_cwt(length: int, widths: tuple, n_coefficients: int) -> None:
     out = cwt_coefficients(
         pl.Series([1 for _ in range(length)]),
