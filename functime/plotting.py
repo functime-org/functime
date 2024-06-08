7564682fa66a79240be6acaf66a70491f97c249f
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-06-08 17:48:33 +0200

    refactor: revert previous PR

--- functime/plotting.py
+++ functime/plotting.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import polars as pl
@@ -11,7 +12,7 @@ from functime.base.metric import METRIC_TYPE
 from functime.metrics import smape
 
 if TYPE_CHECKING:
-    from typing import Any, Dict, Optional, Tuple, Union
+    from typing import Optional, Union
 COLOR_PALETTE = {"actual": "#B7B7B7", "forecast": "#1b57f1", "backtest": "#A76EF4"}
 DEFAULT_LAST_N = 64
 
@@ -24,178 +25,6 @@ def _remove_legend_duplicates(fig: go.Figure) -> go.Figure:
         else names.add(trace.name)
     )
     return fig
-
-
-def _set_subplot_default_kwargs(kwargs: dict, n_rows: int, n_cols: int) -> dict:
-    """
-    Sets or adjusts plot layout properties based on the number of rows and columns in subplots,
-    ensuring a fixed size for subplots and additional space for titles and other elements.
-    Default values are applied only if not already specified by the user.
-
-    Parameters
-    ----------
-    kwargs : dict
-        The original keyword arguments dictionary passed to the plotting function.
-    n_rows : int
-        Number of rows in the subplot.
-    n_cols : int
-        Number of columns in the subplot.
-
-    Returns
-    --------
-    dict : Updated keyword arguments with adjusted layout properties.
-
-    The function ensures the following:
-    - A fixed size for each subplot.
-    - Additional space for plot titles and other elements.
-    - The overall figure size is dynamically adjusted based on the subplot configuration.
-    """
-    # Fixed size for each subplot
-    subplot_width = 250  # width for each subplot column
-    subplot_height = 200  # height for each subplot row
-
-    # Additional space for titles and other elements
-    additional_space_vertical = 100  # space for titles, labels, etc.
-    additional_space_horizontal = 100  # additional horizontal space if needed
-
-    # Calculate total width and height
-    total_width = subplot_width * n_cols + additional_space_horizontal
-    total_height = subplot_height * n_rows + additional_space_vertical
-
-    # Apply defaults if not already specified by the user
-    kwargs.setdefault("width", total_width)
-    kwargs.setdefault("height", total_height)
-    kwargs.setdefault("template", "plotly_white")
-
-    return kwargs
-
-
-def _calculate_subplot_n_rows(n_series: int, n_cols: int) -> int:
-    if n_series <= 0:
-        raise ValueError("n_series must be greater than 0.")
-    if n_cols <= 0:
-        raise ValueError("n_cols must be greater than 0.")
-    n_rows = n_series // n_cols
-    if n_series % n_cols != 0:
-        n_rows += 1
-
-    return n_rows
-
-
-def _get_subplot_grid_position(i: int, n_cols: int) -> Tuple[int, int]:
-    row = i // n_cols + 1
-    col = i % n_cols + 1
-    return row, col
-
-
-def _prepare_data_for_subplots(
-    y: Union[pl.DataFrame, pl.LazyFrame],
-    n_series: int,
-    last_n: int,
-    seed: Union[int, None] = None,
-) -> Tuple[pl.Series, int, pl.DataFrame]:
-    """
-    Prepares data for plotting by selecting and sampling entities and getting the recent observations for plotting.
-
-    Parameters
-    ----------
-    y : Union[pl.DataFrame, pl.LazyFrame]
-        Panel DataFrame of observed values.
-    n_series : int
-        Number of entities / time-series to plot.
-    last_n : int
-        Plot `last_n` most recent values in `y`.
-    seed : Union[int, None], optional
-        Random seed for sampling entities / time-series, by default None.
-
-    Returns
-    -------
-    Tuple[pl.Series, int, pl.DataFrame]
-        Sampled entities, n_series to plot, and filtered DataFrame.
-    """
-    entity_col = y.columns[0]
-
-    if isinstance(y, pl.DataFrame):
-        y = y.lazy()
-
-    # Get unique entities
-    entities = y.select(pl.col(entity_col).unique(maintain_order=True)).collect()
-
-    # If n_series is higher than max unique entities, use max entities
-    if entities.height < n_series:
-        n_series = entities.height
-
-    # Sample entities
-    entities_sample = entities.to_series().sample(n_series, seed=seed)
-
-    # Get most recent observations
-    y_filtered = (
-        y.filter(pl.col(entity_col).is_in(entities_sample))
-        .group_by(entity_col)
-        .tail(last_n)
-        .collect()
-    )
-
-    return entities_sample, n_series, y_filtered
-
-
-def _add_scatter_traces_to_subplots(
-    fig: go.Figure,
-    ts: Union[pl.DataFrame, pl.LazyFrame],
-    ts_pred: Union[pl.DataFrame, pl.LazyFrame, None],
-    entity_id: Any,
-    row: int,
-    col: int,
-    plot_params: Dict[str, Any],
-) -> None:
-    """
-    Adds traces to a specific subplot in the figure for actual and optionally predicted data.
-
-    Parameters
-    ----------
-    fig : go.Figure
-        The Plotly figure object containing the subplots.
-    ts : Union[pl.DataFrame, pl.LazyFrame]
-        The primary time series data to plot.
-    ts_pred : Union[pl.DataFrame, pl.LazyFrame, None]
-        The secondary time series data to plot (e.g., predictions or backtests).
-        If None, only ts is plotted.
-    entity_id : Any
-        The identifier for the current entity being plotted.
-    row : int
-        The row position in the subplot grid.
-    col : int
-        The column position in the subplot grid.
-    plot_params : Dict[str, Any]
-        Dictionary containing parameters for plotting, such as color and name.
-
-    Returns
-    -------
-    None
-    """
-    entity_col, time_col, target_col = ts.columns[:3]
-
-    ts_trace = go.Scatter(
-        x=ts.filter(pl.col(entity_col) == entity_id).get_column(time_col),
-        y=ts.filter(pl.col(entity_col) == entity_id).get_column(target_col),
-        name=plot_params.get("ts_name", "Actual"),
-        legendgroup=plot_params.get("ts_name", "Actual"),
-        line=dict(color=plot_params.get("ts_color", "blue")),
-    )
-
-    fig.add_trace(ts_trace, row=row, col=col)
-
-    if ts_pred is not None:
-        if isinstance(ts_pred, pl.LazyFrame):
-            ts_pred = ts_pred.collect()
-        ts_pred_trace = go.Scatter(
-            x=ts_pred.filter(pl.col(entity_col) == entity_id).get_column(time_col),
-            y=ts_pred.filter(pl.col(entity_col) == entity_id).get_column(target_col),
-            name=plot_params.get("ts_pred_name", "Forecast"),
-            legendgroup=plot_params.get("ts_pred_name", "Forecast"),
-            line=dict(color=plot_params.get("ts_pred_color", "red"), dash="dash"),
-        )
-        fig.add_trace(ts_pred_trace, row=row, col=col)
 
 
 def plot_entities(
@@ -240,14 +69,12 @@ def plot_entities(
         **kwargs,
     )
 
-    return fig
-
 
 def plot_panel(
     y: Union[pl.DataFrame, pl.LazyFrame],
     *,
     n_series: int = 10,
-    seed: Union[int, None] = None,
+    seed: int | None = None,
     n_cols: int = 2,
     last_n: int = DEFAULT_LAST_N,
     **kwargs,
@@ -262,7 +89,7 @@ def plot_panel(
     n_series : int
         Number of entities / time-series to plot.
         Defaults to 10.
-    seed : Union[int, None]
+    seed : int | None
         Random seed for sampling entities / time-series.
         Defaults to None.
     n_cols : int
@@ -277,48 +104,48 @@ def plot_panel(
     figure : plotly.graph_objects.Figure
         Plotly subplots.
     """
-    entity_col = y.columns[0]
+    entity_col, time_col, target_col = y.columns[:3]
 
-    # Get sampled entities, check validity of n_series
-    # and filter the y df to contain last_n values
-    entities_sample, n_series, y_filtered = _prepare_data_for_subplots(
-        y=y,
-        n_series=n_series,
-        last_n=last_n,
-        seed=seed,
+    if isinstance(y, pl.DataFrame):
+        y = y.lazy()
+
+    entities = y.select(pl.col(entity_col).unique(maintain_order=True)).collect()
+
+    entities_sample = entities.to_series().sample(n_series, seed=seed)
+
+    # Get most recent observations
+    y = (
+        y.filter(pl.col(entity_col).is_in(entities_sample))
+        .group_by(entity_col)
+        .tail(last_n)
+        .collect()
     )
 
-    # Define grid and make subplots
-    n_rows = _calculate_subplot_n_rows(n_series=n_series, n_cols=n_cols)
-    fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=entities_sample)
+    # Organize subplots
+    n_rows = n_series // n_cols
+    row_idx = np.repeat(range(n_rows), n_cols)
+    fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=entities)
 
-    # Define default names and colors to be used
-    plot_params = {
-        "ts_name": "Time-series",
-        "ts_color": COLOR_PALETTE["actual"],
-    }
-
-    # Loop and plot each sampled entity
-    for i, entity_id in enumerate(entities_sample):
-        ts = y_filtered.filter(pl.col(entity_col) == entity_id)
-        # Get the subplot position for the ts
-        row, col = _get_subplot_grid_position(i=i, n_cols=n_cols)
-        # Plot trace(s) for the timeseries
-        _add_scatter_traces_to_subplots(
-            fig=fig,
-            ts=ts,
-            ts_pred=None,
-            entity_id=entity_id,
+    for i, entity_id in enumerate(entities):
+        ts = y.filter(pl.col(entity_col) == entity_id)
+        row = row_idx[i] + 1
+        col = i % n_cols + 1
+        # Plot actual
+        fig.add_trace(
+            go.Scatter(
+                x=ts.get_column(time_col),
+                y=ts.get_column(target_col),
+                name="Time-series",
+                legendgroup="Time-series",
+                line=dict(color=COLOR_PALETTE["forecast"]),
+            ),
             row=row,
             col=col,
-            plot_params=plot_params,
         )
 
-    # Set default kwargs for plotting if user did not provide these
-    kwargs = _set_subplot_default_kwargs(kwargs=kwargs, n_rows=n_rows, n_cols=n_cols)
+    template = kwargs.pop("template", "plotly_white")
 
-    # Tidy up the plot
-    fig.update_layout(**kwargs)
+    fig.update_layout(template=template, **kwargs)
     fig = _remove_legend_duplicates(fig)
     return fig
 
@@ -328,7 +155,7 @@ def plot_forecasts(
     y_pred: pl.DataFrame,
     *,
     n_series: int = 10,
-    seed: Union[int, None] = None,
+    seed: int | None = None,
     n_cols: int = 2,
     last_n: int = DEFAULT_LAST_N,
     **kwargs,
@@ -345,7 +172,7 @@ def plot_forecasts(
     n_series : int
         Number of entities / time-series to plot.
         Defaults to 10.
-    seed : Union[int, None]
+    seed : int | None
         Random seed for sampling entities / time-series.
         Defaults to None.
     n_cols : int
@@ -360,49 +187,62 @@ def plot_forecasts(
     figure : plotly.graph_objects.Figure
         Plotly subplots.
     """
-    entity_col = y_true.columns[0]
+    entity_col, time_col, target_col = y_true.columns[:3]
 
-    # Get sampled entities, check validity of n_series
-    # and filter the y df to contain last_n values
-    entities_sample, n_series, y_filtered = _prepare_data_for_subplots(
-        y=y_true,
-        n_series=n_series,
-        last_n=last_n,
-        seed=seed,
+    if isinstance(y_true, pl.DataFrame):
+        y_true = y_true.lazy()
+
+    # Get most recent observations
+    entities = y_true.select(pl.col(entity_col).unique(maintain_order=True)).collect()
+
+    entities_sample = entities.to_series().sample(n_series, seed=seed)
+
+    # Get most recent observations
+    y = (
+        y_true.filter(pl.col(entity_col).is_in(entities_sample))
+        .group_by(entity_col)
+        .tail(last_n)
+        .collect()
     )
 
-    # Define grid and make subplots
-    n_rows = _calculate_subplot_n_rows(n_series=n_series, n_cols=n_cols)
-    fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=entities_sample)
+    # Organize subplots
+    n_rows = n_series // n_cols
+    row_idx = np.repeat(range(n_rows), n_cols)
+    fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=entities)
 
-    plot_params = {
-        "ts_name": "Actual",
-        "ts_color": COLOR_PALETTE["actual"],
-        "ts_pred_name": "Forecast",
-        "ts_pred_color": COLOR_PALETTE["forecast"],
-    }
-
-    for i, entity_id in enumerate(entities_sample):
-        ts = y_filtered.filter(pl.col(entity_col) == entity_id)
+    for i, entity_id in enumerate(entities):
+        ts = y.filter(pl.col(entity_col) == entity_id)
         ts_pred = y_pred.filter(pl.col(entity_col) == entity_id)
-        # Get the subplot position for the ts
-        row, col = _get_subplot_grid_position(i=i, n_cols=n_cols)
-        # Plot trace(s) for the timeseries
-        _add_scatter_traces_to_subplots(
-            fig=fig,
-            ts=ts,
-            ts_pred=ts_pred,
-            entity_id=entity_id,
+        row = row_idx[i] + 1
+        col = i % n_cols + 1
+        # Plot actual
+        fig.add_trace(
+            go.Scatter(
+                x=ts.get_column(time_col),
+                y=ts.get_column(target_col),
+                name="Actual",
+                legendgroup="Actual",
+                line=dict(color=COLOR_PALETTE["actual"]),
+            ),
             row=row,
             col=col,
-            plot_params=plot_params,
+        )
+        # Plot forecast
+        fig.add_trace(
+            go.Scatter(
+                x=ts_pred.get_column(time_col),
+                y=ts_pred.get_column(target_col),
+                name="Forecast",
+                legendgroup="Forecast",
+                line=dict(color=COLOR_PALETTE["forecast"], dash="dash"),
+            ),
+            row=row,
+            col=col,
         )
 
-    # Set default kwargs for plotting if user did not provide these
-    kwargs = _set_subplot_default_kwargs(kwargs=kwargs, n_rows=n_rows, n_cols=n_cols)
+    template = kwargs.pop("template", "plotly_white")
 
-    # Tidy up the plot
-    fig.update_layout(**kwargs)
+    fig.update_layout(template=template, **kwargs)
     fig = _remove_legend_duplicates(fig)
     return fig
 
@@ -412,7 +252,7 @@ def plot_backtests(
     y_preds: pl.DataFrame,
     *,
     n_series: int = 10,
-    seed: Union[int, None] = None,
+    seed: int | None = None,
     n_cols: int = 2,
     last_n: int = DEFAULT_LAST_N,
     **kwargs,
@@ -429,7 +269,7 @@ def plot_backtests(
     n_series : int
         Number of entities / time-series to plot.
         Defaults to 10.
-    seed : Union[int, None]
+    seed : int | None
         Random seed for sampling entities / time-series.
         Defaults to None.
     n_cols : int
@@ -444,49 +284,62 @@ def plot_backtests(
     figure : plotly.graph_objects.Figure
         Plotly subplots.
     """
-    entity_col = y_true.columns[0]
+    entity_col, time_col, target_col = y_true.columns[:3]
 
-    # Get sampled entities, check validity of n_series
-    # and filter the y df to contain last_n values
-    entities_sample, n_series, y_filtered = _prepare_data_for_subplots(
-        y=y_true,
-        n_series=n_series,
-        last_n=last_n,
-        seed=seed,
+    if isinstance(y_true, pl.DataFrame):
+        y_true = y_true.lazy()
+
+    # Get most recent observations
+    entities = y_true.select(pl.col(entity_col).unique(maintain_order=True)).collect()
+
+    entities_sample = entities.to_series().sample(n_series, seed=seed)
+
+    # Get most recent observations
+    y = (
+        y_true.filter(pl.col(entity_col).is_in(entities_sample))
+        .group_by(entity_col)
+        .tail(last_n)
+        .collect()
     )
 
-    # Define grid and make subplots
-    n_rows = _calculate_subplot_n_rows(n_series=n_series, n_cols=n_cols)
-    fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=entities_sample)
+    # Organize subplots
+    n_rows = n_series // n_cols
+    row_idx = np.repeat(range(n_rows), n_cols)
+    fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=entities)
 
-    plot_params = {
-        "ts_name": "Actual",
-        "ts_color": COLOR_PALETTE["actual"],
-        "ts_pred_name": "Backtest",
-        "ts_pred_color": COLOR_PALETTE["backtest"],
-    }
-
-    for i, entity_id in enumerate(entities_sample):
-        ts = y_filtered.filter(pl.col(entity_col) == entity_id)
+    for i, entity_id in enumerate(entities):
+        ts = y.filter(pl.col(entity_col) == entity_id)
         ts_pred = y_preds.filter(pl.col(entity_col) == entity_id)
-        # Get the subplot position for the ts
-        row, col = _get_subplot_grid_position(i=i, n_cols=n_cols)
-        # Plot trace(s) for the timeseries
-        _add_scatter_traces_to_subplots(
-            fig=fig,
-            ts=ts,
-            ts_pred=ts_pred,
-            entity_id=entity_id,
+        row = row_idx[i] + 1
+        col = i % n_cols + 1
+        # Plot actual
+        fig.add_trace(
+            go.Scatter(
+                x=ts.get_column(time_col),
+                y=ts.get_column(target_col),
+                name="Actual",
+                legendgroup="Actual",
+                line=dict(color=COLOR_PALETTE["actual"]),
+            ),
             row=row,
             col=col,
-            plot_params=plot_params,
+        )
+        # Plot forecast
+        fig.add_trace(
+            go.Scatter(
+                x=ts_pred.get_column(time_col),
+                y=ts_pred.get_column(target_col),
+                name="Backtest",
+                legendgroup="Backtest",
+                line=dict(color=COLOR_PALETTE["backtest"], dash="dash"),
+            ),
+            row=row,
+            col=col,
         )
 
-    # Set default kwargs for plotting if user did not provide these
-    kwargs = _set_subplot_default_kwargs(kwargs=kwargs, n_rows=n_rows, n_cols=n_cols)
+    template = kwargs.pop("template", "plotly_white")  # noqa: F841
 
-    # Tidy up the plot
-    fig.update_layout(**kwargs)
+    fig.update_layout(template=template, **kwargs)
     fig = _remove_legend_duplicates(fig)
     return fig
 
@@ -521,7 +374,7 @@ def plot_residuals(
     y_resids = y_resids.with_columns(pl.col(target_col).alias("Residuals")).collect()
 
     fig = px.histogram(
-        y_resids.to_pandas(),
+        y_resids,
         x="Residuals",
         y="Residuals",
         color=entity_col,
@@ -577,10 +430,7 @@ def plot_comet(
     mean_score = scores.get_column(scores.columns[-1]).mean()
     mean_cv = cvs.get_column(cvs.columns[-1]).mean()
     fig = px.scatter(
-        comet.to_pandas(),
-        x=cvs.columns[-1],
-        y=scores.columns[-1],
-        hover_data=entity_col,
+        comet, x=cvs.columns[-1], y=scores.columns[-1], hover_data=entity_col
     )
     fig.add_hline(y=mean_score)
     fig.add_vline(x=mean_cv)
@@ -629,7 +479,7 @@ def plot_fva(
         how="left",
         on=scores.columns[0],
     )
-    fig = px.scatter(uplift.to_pandas(), x=x_title, y=y_title, hover_data=entity_col)
+    fig = px.scatter(uplift, x=x_title, y=y_title, hover_data=entity_col)
     deg45_line = {
         "type": "line",
         "yref": "paper",
